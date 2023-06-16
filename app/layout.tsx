427634f0b2a075329de91c924900b82acdbb0a28
Iionsroar <yawned.eyes@gmail.com>
2023-06-17 05:22:14 +0800

    feat: add dark mode toggle

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
 import Link from 'next/link'
-import { Providers } from './providers'
+import { Providers, ThemeChanger } from './providers'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,7 +19,18 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
         <Providers>
-          <main className="flex min-h-screen flex-col items-center justify-between p-24">
+          <main className="bg-orange-50 dark:bg-zinc-900 dark:text-orange-50 flex min-h-screen flex-col items-center justify-between p-24">
+            <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+              <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+                You&apos;re &nbsp;
+                <code className="font-mono font-bold">here!</code>
+              </p>
+
+              <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+                <ThemeChanger />
+              </div>
+            </div>
+
             {children}
 
             <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
--- app/page.tsx
+++ app/page.tsx
@@ -8,21 +8,13 @@ export const metadata: Metadata = {
 
 export default function Home() {
   return (
-    <>
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          You are in the &nbsp;
-          <code className="font-mono font-bold">home page!</code>
-        </p>
-      </div>
-      <div className="relative flex place-items-center">
-        <Image
-          src="/rabbit.png"
-          alt="Rabbit"
-          width={214}
-          height={300}
-        />
-      </div>
-    </>
+    <div className="relative flex place-items-center">
+      <Image
+        src="/rabbit.png"
+        alt="Rabbit"
+        width={214}
+        height={300}
+      />
+    </div>
   )
 }
