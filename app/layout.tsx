8d6c1fbc33ade46911a4fc287ff582d0d51ddd87
Iionsroar <yawned.eyes@gmail.com>
2023-06-19 07:24:55 +0800

    style: change letterboxing height for dark mode; fix themechanger not showing

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,15 +23,15 @@ export default function RootLayout({
           <main className="h-screen bg-white dark:bg-zinc-900 dark:text-akairosu-white flex">
             <GlobalNav />
             <div className="flex flex-col">
-              <div className="invisible lg:visible w-screen h-10 border-b-4 bg-zinc-200 dark:bg-akairosu-white border-zinc-300 dark:border-akairosu-orange"></div>
-              <div className="lg:pl-80 h-full flex">
+              <div className="invisible lg:visible w-screen h-10 dark:h-8 border-b-4 bg-zinc-200 dark:bg-akairosu-white border-zinc-300 dark:border-akairosu-orange"></div>
+              <div className="lg:pl-80 w-full h-full flex">
                 <div className="mx-auto px-2 pt-20 lg:p-0 flex">
                   {/* had to add lg:left-auto to make it work both sides */}
                   <ThemeChanger className="fixed lg:top-14 lg:right-6 lg:left-auto left-3 top-3 z-10" />
                   {children}
                 </div>
               </div>
-              <div className="bottom-0 invisible lg:visible w-screen h-10 bg-zinc-200 dark:bg-akairosu-white border-t-4 border-zinc-300 dark:border-akairosu-orange"></div>
+              <div className="bottom-0 invisible lg:visible w-screen h-10 dark:h-8 bg-zinc-200 dark:bg-akairosu-white border-t-4 border-zinc-300 dark:border-akairosu-orange"></div>
             </div>
           </main>
         </Providers>
