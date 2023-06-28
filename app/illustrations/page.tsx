1cfad76c72e0325db98b4e2e527cc4f2a35c791d
Iionsroar <yawned.eyes@gmail.com>
2023-06-28 11:10:23 +0800

    style: fix gallery layout; remove transition for nav texts

--- app/illustrations/page.tsx
+++ app/illustrations/page.tsx
@@ -13,20 +13,20 @@ const Home: NextPage = async () => {
   const images: ImageProps[] = await getImages()
 
   return (
-    <main className="relative h-full grid place-content-center">
-      <div className="flex h-full w-full">
+    <main className="relative h-full p-4">
+      <div className="flex flex-wrap gap-2 h-full absolute overflow-y-auto">
         {/* <span className="text-2xl pr-4 mr-4 border-e-[1px] border-zinc-400">🐐</span>
         <span>Illustrations</span> */}
         {images.map(({ id, public_id, format, height, width }) => (
           <div key={id}>
             <Image
               alt="Overwatch fanart by @akairosu_"
-              className="transform rounded-lg brightness-90 transition will-change-auto group-hover:brightness-110"
+              className="rounded-lg"
               // placeholder="blur"
               // blurDataURL={blurDataUrl}
               src={`https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/c_scale,w_720/${public_id}.${format}`}
-              width={width / 7}
-              height={height / 7}
+              width={width / 6}
+              height={height / 6}
             />
           </div>
           ))}
