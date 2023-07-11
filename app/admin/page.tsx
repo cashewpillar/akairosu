dcc1ea8ec0a72d7eb55bc729c1316b1886b81f8b
Iionsroar <yawned.eyes@gmail.com>
2023-07-11 16:16:10 +0800

    chore: read demos

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -4,6 +4,7 @@ import { authOptions } from '@/lib/auth'
 import { Redeploy } from '@/ui/redeploy'
 import { SignIn } from '@/ui/sign-in'
 import { SignOut } from '@/ui/sign-out'
+import { demos } from '@/lib/demos'
 
 export const metadata: Metadata = {
   title: 'Admin',
@@ -26,7 +27,7 @@ const Home: NextPage = async () => {
       ):(
         <div className="px-4 py-8 flex flex-col m-8 space-y-8 h-[90%] bg-zinc-100 border-b-4 rounded-lg border-zinc-400/30 dark:bg-zinc-800/30 dark:border-zinc-700">
           <table className="table-auto text-left">
-            <thead>
+            <thead className="text-akairosu-brown">
               <tr>
                 <th>Action</th>
                 <th>Function</th>
@@ -48,19 +49,19 @@ const Home: NextPage = async () => {
 
           <table className="table-auto text-left">
             <caption className='caption-bottom text-xs text-zinc-400 dark:text-zinc-500 text-left'>Quick tip: hover over the header texts to learn what each column means!</caption>
-            <thead>
+            <thead className='text-akairosu-brown'>
               <tr>
                 <th title="your portfolio's pages">Page</th>
-                <th title='name of the tab'>Title</th>
                 <th title='helps search engines to index the website so they can show up top!'>Description</th>
               </tr>
             </thead>
             <tbody>
-              <tr>
-                <td>About</td>
-                <td>akairosu_</td>
-                <td>🐐 garden goat phvtuber · art · live2d</td>
-              </tr>
+              {demos.map((item) => (
+                <tr key={item.slug}>
+                  <td>{ item.name }</td>
+                  <td>{ item.desc }</td>
+                </tr>
+              ))}
             </tbody>
           </table>
         </div>
