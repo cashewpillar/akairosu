d49296f4087d50c6aaf58901ede5b321eba84cbf
Iionsroar <yawned.eyes@gmail.com>
2023-07-21 14:59:15 +0800

    style: mobile about

--- app/page.tsx
+++ app/page.tsx
@@ -20,8 +20,8 @@ const Home: NextPage = async () => {
         so that it is unclickable where the screentones are present
       */}
       <div className="absolute top-0 left-[-12%] bg-none xl:w-8/12 lg:w-9/12 h-full skew-x-[167deg]"></div>
-      <div className="lg:visible invisible absolute top-0 left-0 h-full xl:w-7/12 w-1/2 pl-12 pr-28 lg:py-16 py-8 ">
-        <div className="flex flex-col place-content-center text-sm text-center leading-loose px-20 w-full h-full rounded-lg">
+      <div className="pointer-events-none absolute top-0 left-0 h-full xl:w-7/12 lg:w-1/2 w-full lg:pl-12 lg:pr-28 px-20 lg:py-16 py-8">
+        <div className="text-akairosu-white lg:text-zinc-900 dark:lg:text-akairosu-white flex flex-col place-content-center text-sm text-center leading-loose xl:px-20 w-full h-full rounded-lg">
           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore, fuga veniam. Placeat beatae quis accusamus, in quae libero adipisci, cum cumque rerum dignissimos nam cupiditate, assumenda necessitatibus. Perferendis, eaque ipsum?
         </div>
       </div>
