3ebe461b8f5366c5ee3dd15769738bab639b74df
Iionsroar <yawned.eyes@gmail.com>
2023-07-29 10:50:12 +0800

    chore: 404 not found page

---
+++ app/not-found.tsx
@@ -0,0 +1,10 @@
+export default function NotFound() {
+  return (
+    <main className="relative h-full grid place-content-center">
+      <div className="dark:text-white">
+        <span className="text-xl pr-4 py-4 mr-4 border-e-[1px] border-zinc-400">4🐐4</span>
+        <span className="mr-4 text-sm">This page could not be found.</span>
+      </div>
+    </main>
+  )
+}
